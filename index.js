0d1bd83ee7729c9884f558cf7737d2c967197d19
Rose <r.d.jeanlouis01@gmail.com>
2021-04-08 21:20:51 -0400

    commit tree 3ee2155d1fd96f82df8274532a2f01f3e065682c

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(aNumber){
+  if (aNumber <= 400)
+  return("This one is on me!");
+  else if (aNumber <= 2499)
+  return("I will gladly take your thirty bucks.");
+  else (aNumber >= 2500)
+  return("No can do.");
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(aCity){
+  return aCity === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(aTip){
+  switch(aTip){
+    case 'generous':
+      return ("Thank you so much.");
+      break;
+    case 'not as generous':
+      return ("Thank you.");
+      break;   
+    default:
+    case 'lastResort':
+      return ("Bye.")
+      break;
+    }
+
 }
